dbdd9043743bbc5cfea2d4757697b5bd1f316bfc
keghar <108595664+keghar@users.noreply.github.com>
2024-01-28 20:27:22 -0600

    added routes

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -16,14 +16,22 @@ export const repositoryName =
 // TODO: Update the routes array to match your project's route structure.
 const routes: prismic.ClientConfig['routes'] = [
   // Examples:
-  // {
-  // 	type: "homepage",
-  // 	path: "/",
-  // },
-  // {
-  // 	type: "page",
-  // 	path: "/:uid",
-  // },
+   {
+   	type: "homepage",
+  	path: "/",
+  },
+  {
+    type: "camp",
+   path: "/camp",
+ },
+ {
+  type: "about",
+ path: "/about",
+},
+  {
+  	type: "event",
+  	path: "/archive/:uid",
+  },
 ]
 
 /**
